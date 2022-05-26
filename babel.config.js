85f0d14b287498e0474c83bff5335ba45b11a06e
Gustavo Henrique <gugahribeiro@hotmail.com>
2022-05-26 19:01:51 -0300

    chore: config absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.js', '.jsx', '.es', '.es6', '.mjs', '.ts', '.tsx', '.svg', '.png', '.cjs'],
+          root: ['./'],
+          alias: {
+            '@modules': './src/modules',
+            '@shared': './src/shared',
+          }
+        }
+      ]
+    ]
   };
 };
